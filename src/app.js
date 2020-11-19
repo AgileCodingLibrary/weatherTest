f0795c93327c9a0651b0ced593291d692cccbc1c
faz ahmed <fazahmed20xx@hotmail.com>
2020-11-19 18:47:57 +0000

    updated me photo

--- src/app.js
+++ src/app.js
@@ -88,7 +88,7 @@ app.get("/products", (req, res) => {
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Faz Ahmed",
     errorMessage: "Help article not found.",
   });
 });
@@ -96,7 +96,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Faz Ahmed",
     errorMessage: "Page not found.",
   });
 });
